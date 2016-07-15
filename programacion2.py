c9621add68d6ce51134cd9ccd306026d6f50fc50
julio1411 <pinkichin123456julitto_14@hotmail.com>
2016-07-14 20:54:50 -0500

    agregando suma
    mostrando resultados

--- programacion2.py
+++ programacion2.py
@@ -2,3 +2,13 @@ print("")
 print("hola mundo")
 print("bienvienvenidos a Github")
 print("La mejor plataforma para realizar trabajos colaborativos :D")
+
+
+print("Hallar la Suma de los siguientes Numeros")
+
+a=int(input("Numero: "))
+b=int(input("Numero: "))
+
+c=a+b
+
+print("la Suma es: ",c)
